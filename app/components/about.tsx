692cf54ea1364e402c85a0bb4c3e18fc8cd7ee65
Najaf <najafshaikh1705@gmail.com>
2025-01-22 21:17:54 +0530

    icons

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function About() {
   return (
     <motion.section
-      className="flex flex-col justify-center items-center min-h-[100vh] m-8 sm:mx-28 lg:scroll-mt-40 md:scroll-mt-16 text-gray-700 dark:text-gray-100"
+      className="flex flex-col justify-center items-center min-h-[100vh] m-8 sm:mx-28 text-gray-700 dark:text-gray-100"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.155 }}
