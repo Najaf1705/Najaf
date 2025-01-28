f53ee7be58dd2cd7bb2966747b86a186b33b9eef
Najaf <najafshaikh1705@gmail.com>
2025-01-29 00:06:18 +0530

    admin

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,19 +1,150 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { firestore, doc, getDoc } from "../lib/firebase"; // Correctly import doc and getDoc
+import toast from "react-hot-toast";
+
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
+  const documentId = process.env.NEXT_PUBLIC_FIREBASE_DOCUMENT_ID;
+
+  // State variables
+  const [isPasswordDialogOpen, setPasswordDialogOpen] = useState(false);
+  const [isTableDialogOpen, setTableDialogOpen] = useState(false);
+  const [password, setPassword] = useState("");
+  const [tableData, setTableData] = useState<any[]>([]);
+
+  // Fetch data from Firestore for the specific document
+  const fetchData = async () => {
+    try {
+      const docRef = doc(firestore, "clicks_data", documentId); // Using the env var for document ID
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        
+        const formattedData = Object.keys(data).map((key) => ({
+          link: key,
+          count: data[key],
+        }));
+
+        setTableData(formattedData); // Set the formatted data in the state
+      } else {
+        console.log("No such document!");
+      }
+    } catch (error) {
+      console.error("Error fetching document: ", error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch data when component mounts
+    fetchData();
+  }, []);
+
+  // Handle admin button click
+  const handleAdminClick = () => {
+    setPassword(""); // Reset password field when opening the dialog
+    setPasswordDialogOpen(true);
+  };
+
+  // Handle password dialog submission
+  const handlePasswordSubmit = () => {
+    if (password === process.env.NEXT_PUBLIC_ADMIN_PASS) {
+      setPasswordDialogOpen(false);
+      setTableDialogOpen(true);
+    } else {
+      // alert("Incorrect password!");
+      toast.error("Incorrect password!");
+    }
+  };
+
+  // Use useEffect to add/remove the no-scroll class on the body
+  useEffect(() => {
+    if (isPasswordDialogOpen || isTableDialogOpen) {
+      // Add no-scroll class when dialog is open
+      document.body.classList.add("overflow-hidden");
+    } else {
+      // Remove no-scroll class when dialog is closed
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isPasswordDialogOpen, isTableDialogOpen]);
 
   return (
     <div>
-      <footer className="px-4 mt-20 text-center text-white dark:text-black bg-black dark:bg-gray-400">
-        <small className="mb-2 block text-xs">
+      <button onClick={handleAdminClick}>
+        <small>Admin</small>
+      </button>
+
+      {/* Password Dialog */}
+      {isPasswordDialogOpen && (
+        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-70 z-50">
+          <div className="bg-gray-600 p-6 rounded-lg shadow-lg max-w-sm w-full animate-scale-up">
+            <h3 className="text-lg font-semibold mb-4 text-black">Password</h3>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <div className="flex justify-end gap-2">
+              <button
+                onClick={handlePasswordSubmit}
+                className="text-white bg-black dark:text-black dark:bg-white border-black/5 py-1 px-2 rounded-xl flex items-center gap-2"
+              >
+                Submit
+              </button>
+              <button
+                onClick={() => setPasswordDialogOpen(false)}
+                className="bg-gray-300 text-gray-700 p-2 rounded hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Table Section */}
+      {isTableDialogOpen && (
+        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-70 z-50 text-black">
+          <div className="bg-gray-600 p-6 rounded-lg shadow-lg max-w-sm w-full animate-scale-up">
+            <h3 className="text-lg font-semibold mb-4">Clicks</h3>
+            <div className="mb-4 overflow-x-auto">
+              <table className="w-full table-auto text-sm text-white">
+                <thead>
+                  <tr>
+                    <th className="border-b border-gray-400 p-2 text-left">Link</th>
+                    <th className="border-b border-gray-400 p-2 text-left">clicks</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableData.map((row, index) => (
+                    <tr key={index}>
+                      <td className="border-b border-gray-400 p-2">{row.link}</td>
+                      <td className="border-b border-gray-400 p-2">{row.count}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="flex justify-end gap-2">
+              <button
+                onClick={() => setTableDialogOpen(false)} // Close the table dialog
+                className="bg-black text-white p-2 rounded hover:bg-gray-800"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <footer className="px-4 text-center text-white dark:text-black bg-black dark:bg-gray-400">
+        <small className="block text-xs">
           &copy; {currentYear} Najaf Shaikh. All rights reserved.
         </small>
-        {/* <p className="text-xs">
-          <span className="font-semibold">About this website:</span> built with
-          React & Next.js (App Router & Server Actions), TypeScript, Tailwind
-          CSS, Framer Motion, React Email & Resend, Vercel hosting.
-        </p> */}
       </footer>
     </div>
   );
--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -9,8 +9,26 @@ import { GoArrowRight } from "react-icons/go";
 import { LuDownload } from "react-icons/lu";
 import { FaCode, FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
+import { getFirestore, doc, updateDoc, increment } from "firebase/firestore";
+
+// Firestore instance
+const firestore = getFirestore();
 
 export default function Intro() {
+  const documentId = process.env.NEXT_PUBLIC_FIREBASE_DOCUMENT_ID;
+
+  const incrementCount = async (link: string) => {
+    try {
+      const docRef = doc(firestore, "clicks_data", documentId); // Replace with your document ID
+      await updateDoc(docRef, {
+        [link]: increment(1), // Increment the count by 1
+      });
+      console.log(`${link} count incremented successfully`);
+    } catch (error) {
+      console.error("Error incrementing count: ", error);
+    }
+  };
+
   const TextAnimatedGradient = () => {
     return (
       <span className="font-extrabold text-3xl inline-flex animate-text-gradient bg-gradient-to-r from-[#00FF88] via-[#00B5E2] to-[#00FF88]
@@ -78,70 +96,74 @@ export default function Intro() {
             <GoArrowRight />{" "}
           </motion.a> */}
           <motion.a
-            href="/najafCV.pdf"
-            download
-            className="hover:bg-gray-600 dark:hover:bg-gray-300  shadow-sm shadow-black/10 cursor-pointer font-medium text-white bg-black dark:text-black dark:bg-white flex items-center py-3 px-7 
-          gap-2 justify-center relative rounded-full "
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.2,
-            }}
-          >
-            {" "}
-            Download Resume
-            <LuDownload />
-          </motion.a>
+        href="/najafCV.pdf"
+        download
+        className="hover:bg-gray-600 dark:hover:bg-gray-300 shadow-sm shadow-black/10 cursor-pointer font-medium text-white bg-black dark:text-black dark:bg-white flex items-center py-3 px-7 gap-2 justify-center relative rounded-full"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.2,
+        }}
+        onClick={() => incrementCount("cv")} // Increment LinkedIn count
+      >
+        Download Resume
+        <LuDownload />
+      </motion.a>
 
+      <motion.div className="flex gap-2">
+        {/* LinkedIn Icon */}
+        <motion.a
+          href="https://www.linkedin.com/in/najaf17/"
+          className="text-3xl text-white bg-black dark:text-black dark:bg-white p-3 gap-2 rounded-full flex items-center shadow-sm shadow-black/10 justify-center relative group"
+          target="_blank"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+          }}
+          onClick={() => incrementCount("linkedin")} // Increment LinkedIn count
+        >
+          <FaLinkedin className="hover:scale-105" />
+          <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white rounded-lg p-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+            LinkedIn
+          </div>
+        </motion.a>
 
-          <motion.div className="flex gap-2">
-            <motion.a
-              href="https://www.linkedin.com/in/najaf17/"
-              className="text-3xl text-white bg-black dark:text-black dark:bg-white p-3 gap-2 rounded-full flex items-center shadow-sm shadow-black/10 justify-center relative group"
-              target="_blank"
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0.3,
-              }}
-            >
-              <FaLinkedin className="hover:scale-105" />
-              <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white rounded-lg p-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
-                LinkedIn
-              </div>
-            </motion.a>
+        {/* GitHub Icon */}
+        <motion.a
+          href="https://github.com/najaf1705"
+          className="text-3xl text-white bg-black dark:text-black dark:bg-white shadow-sm shadow-black/10 p-3 gap-2 flex items-center justify-center relative rounded-full group"
+          target="_blank"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.4,
+          }}
+          onClick={() => incrementCount("github")} // Increment GitHub count
+        >
+          <FaGithubSquare className="hover:scale-105" />
+          <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white rounded-lg p-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+            GitHub
+          </div>
+        </motion.a>
 
-            <motion.a
-              href="https://github.com/najaf1705"
-              className="text-3xl text-white bg-black dark:text-black dark:bg-white shadow-sm shadow-black/10 p-3 gap-2 flex items-center justify-center relative rounded-full group"
-              target="_blank"
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0.4,
-              }}
-            >
-              <FaGithubSquare className="hover:scale-105" />
-              <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white rounded-lg p-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
-                GitHub
-              </div>
-            </motion.a>
-
-            <motion.a
-              href="https://leetcode.com/enzoe/"
-              className="text-3xl text-white bg-black dark:text-black dark:bg-white shadow-sm shadow-black/10 p-3 gap-2 flex items-center justify-center relative rounded-full group"
-              target="_blank"
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0.4,
-              }}
-            >
-              <FaCode className="hover:scale-105" />
-              <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white rounded-lg p-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
-                Leetcode
-              </div>
-            </motion.a>
+        {/* Leetcode Icon */}
+        <motion.a
+          href="https://leetcode.com/enzoe/"
+          className="text-3xl text-white bg-black dark:text-black dark:bg-white shadow-sm shadow-black/10 p-3 gap-2 flex items-center justify-center relative rounded-full group"
+          target="_blank"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.4,
+          }}
+          onClick={() => incrementCount("cp")} // Increment Leetcode count
+        >
+          <FaCode className="hover:scale-105" />
+          <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white rounded-lg p-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+            Leetcode
+          </div>
+        </motion.a>
 
           </motion.div>
         </div>
